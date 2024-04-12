1a55fb554ebc6179dc2cfa0170301106a2863a1f
Emmanuel Mejia <emmanuel.mejia.jimenez@gmail.com>
2024-04-12 13:03:54 -0600

    Update main.py
    Description format

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 """
+
 2024.04.01
     1. Vamos a crear una función para sumar dos número enteros
-
     Los siguientes pasos los vamos a ejecutar desde el shell interactivo de ipython
     2. Cargamos el archivo con el comando,
         %load main.py
         Una vez cargado podremos utilizar todo el contenido del archivo (clases, variables, métodos, funciones, etc)
-    
     3. Podemos verificar la documentación de la función, con el comando,
         %pdoc suma
-    
     4. Ejecutamos la función cargada en el shell de ipython, llamando a la función
         suma(10,30)
 
